94124c50f1550f9839dc7220a991cc3f2acd8cdc
clap_min99 <qkrtnals032@gmail.com>
2024-07-15 17:11:42 +0900

    modified

---
+++ 240715_hw/day1_3.py
@@ -0,0 +1,10 @@
+twinkle= '반짝 반짝'
+on_ = '에서도'
+little_star= '작은 별'
+shine_='아름답게 비치네'
+east_sky='동쪽 하늘'
+west_sky='서쪽 하늘'
+
+print(twinkle,little_star,shine_)
+print(east_sky+on_,west_sky+on_)
+print(twinkle,little_star,shine_)
---
+++ 240715_hw/ws_1_b.py
@@ -0,0 +1,8 @@
+# 아래에 코드를 작성하시오.
+korean = '한글'
+english = 'english'
+number = 3
+
+print(f'{korean}과 {english}')
+print(f'{korean*3}')
+      
