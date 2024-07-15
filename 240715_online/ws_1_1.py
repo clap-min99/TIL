3430068db069886aaaa9900a935162c54b5fcea1
clap_min99 <qkrtnals032@gmail.com>
2024-07-15 17:07:22 +0900

    done

---
+++ 240715_online/ws_1_1.py
@@ -0,0 +1,13 @@
+name = '홍길동'
+age = 20
+name_input = '이름 : '
+age_input = '나이 : '
+stu_info = '학생 정보를 출력합니다.'
+each_info = '각각의 정보는 다음과 같습니다'
+finished = '출력 완료'
+
+print(stu_info)
+print(each_info)
+print(name_input, name)
+print(age_input, age)
+print(finished)
