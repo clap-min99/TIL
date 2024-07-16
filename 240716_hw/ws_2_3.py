6b89f71cd78178289589147a7c557fec74d83387
clap_min99 <qkrtnals032@gmail.com>
2024-07-16 17:34:23 +0900

    240716

---
+++ 240716_hw/ws_2_3.py
@@ -0,0 +1,4 @@
+books = ['광문자전', '유연전', '심청전', '홍길동전', '수성지']
+authors = ['작자 미상', '허균', '박지원', '이항복', '임제']
+
+print(f'{authors[1]} : {books[3]}\n{authors[3]} : {books[1]}\n{authors[0]} : {books[2]}\n{authors[2]} : {books[0]}\n{authors[4]} : {books[4]}\n')
---
+++ 240716_hw/ws_2_c.py
@@ -0,0 +1,65 @@
+data = [{'has_more': False,
+  'next_cursor': None,
+  'object': 'list',
+  'page_or_database': {},
+  'request_id': 'a5163fff-758f-45ea-b6fb',
+  'results': [{'archived': False,
+               'cover': None,
+               'created_by': {'object': 'user'},
+               'created_time': '2023-06-15T04:29:00.000Z',
+               'icon': None,
+               'last_edited_by': {'object': 'user'},
+               'last_edited_time': '2023-12-12T09:19:00.000Z',
+               'object': 'page',
+               'parent': {'type': 'database_id'},
+               'properties': {'setNum': {'id': '%7DK%40%5C',
+                                         'number': 1,
+                                         'type': 'number'},
+                              '과목': {'id': 'YuIE',
+                                     'multi_select': [{'color': 'default',
+                                                       'name': 'Python'}],
+                                     'type': 'multi_select'},
+                              '구분': {'id': '%40%3EmR',
+                                     'select': {'color': 'purple',
+                                                'name': '실습'},
+                                     'type': 'select'},
+                              '단계': {'id': 'T%7B%7BP',
+                                     'select': {'color': 'default',
+                                                'name': '3'},
+                                     'type': 'select'},
+                              '문제번호': {'id': 'uEBt',
+                                       'number': 1431,
+                                       'type': 'number'},
+                              '제목': {'id': 'title',
+                                     'title': [{'annotations': {'bold': False,
+                                                                'code': False,
+                                                                'color': 'default',
+                                                                'italic': False,
+                                                                'strikethrough': False,
+                                                                'underline': False},
+                                                'href': None,
+                                                'plain_text': '복잡한 자료구조',
+                                                'text': {'content': '복잡한 자료구조',
+                                                         'link': None},
+                                                'type': 'text'}],
+                                     'type': 'title'},
+                              '일차': {'id': 'nWnH',
+                                     'number': '2',
+                                     'type': 'number'},
+                              '커리큘럼': {'id': 'T%3AR_',
+                                       'multi_select': [{'color': 'default',
+                                                         'name': 'fundamentals-of-python'}],
+                                       'type': 'multi_select'}},
+               'public_url': None
+            }],
+  'type': 'page_or_database'}]
+
+# 아래에 코드를 작성하시오.
+
+first_data = {}
+first_data['제목']= data[0]['results'][0]['properties']['제목']['title'][0]['plain_text']
+first_data['일차']= int(data[0]['results'][0]['properties']['일차']['number'])
+first_data['단계']= str(int(data[0]['results'][0]['properties']['단계']['select']['name'])) +'단계'
+first_data['과목']= data[0]['results'][0]['properties']['과목']['multi_select'][0]['name']
+
+print(first_data)
