ef4f1022f20512fe601d935c6fc1ff7186f7e7b9
clap_min99 <qkrtnals032@gmail.com>
2024-07-18 19:59:27 +0900

    modified

--- 240717_hw_/ws_3_4.py
+++ 240717_hw_/ws_3_4.py
@@ -9,8 +9,8 @@ def increase_user():
 def create_user(name, age, address):
     increase_user()
     user_info = {'name': name, 'age': age, 'address': address}
-    print(f'{name}님 환영합니다.')
-    return user_info
+    print(f'{name}님 환영합니다!')
+    
     
 
 
