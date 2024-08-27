07180d4d2523efd8fa0eebd8ca5b23aa34a19f0f
clap_min99 <qkrtnals032@gmail.com>
2024-08-28 08:04:52 +0900

    tree

---
+++ Algorithm/algo_note/240827/BJ_트리순회.py
@@ -0,0 +1,38 @@
+preorder= []
+inorder = []
+postorder = []
+
+def search(node):
+    if node != 0:
+        preorder.append(node)
+        search(tree[node][0])
+        inorder.append(node)
+        search(tree[node][1])
+        postorder.append(node)
+
+N = int(input())
+arr = [list(input().split()) for _ in range(N)]
+
+alp_to_num = {}
+num_to_alp = {}
+for i in range(1, N+1):
+    alp_to_num[arr[i-1][0]] = i  # dictionary에 각 알파벳을 숫자로 저장
+    num_to_alp[i] = arr[i-1][0]
+alp_to_num['.'] = 0
+print(alp_to_num)
+
+
+tree = [[0, 0] for _ in range(N+1)]
+for i in range(1, N+1):  # tree 만들기
+    tree[i][0] = alp_to_num[arr[i-1][1]]
+    tree[i][1] = alp_to_num[arr[i-1][2]]
+
+search(1)
+for i in range(N):
+    preorder[i] = num_to_alp[preorder[i]]
+    inorder[i] = num_to_alp[inorder[i]]
+    postorder[i] = num_to_alp[postorder[i]]
+
+print(''.join(preorder))
+print(''.join(inorder))
+print(''.join(postorder))
---
+++ Algorithm/algo_note/240827/oasis.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+N = int(input())
+arr = deque(int(input()) for _ in range(N))
+
+
+stack = []
+stack.append(arr.popleft())
+cnt = 0
+i = 0
+
+while len(arr) != 1:
+    if len(arr) == 1:
+        break
+    if stack[0] < arr[i]:
+        print(stack[0])           
+        # stack[0]는 기준 숫자. arr[i]랑 비교.
+        cnt += 1
+        stack.pop()
+        stack.append(arr.popleft())
+        i = 0
+        continue
+    elif stack[0] >= arr[i]:
+        cnt += 1
+        i += 1
+        if i == len(arr):
+            i = 0
+            continue
+print(arr)
+print(cnt)
+
