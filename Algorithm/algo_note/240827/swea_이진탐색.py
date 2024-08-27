a125aee1f2d3320c9a56d99a09299c300842fbb9
clap_min99 <qkrtnals032@gmail.com>
2024-08-27 17:42:19 +0900

    tree

--- Algorithm/algo_note/240827/swea_이진탐색.py
+++ Algorithm/algo_note/240827/swea_이진탐색.py
@@ -6,12 +6,15 @@ for tc in range(1, T+1):
     N = int(input())
     
     tree = [[0,0] for _ in range(N+1)]
+
     for i in range(1, (N)//2+1):
-        tree[i][0] = 2*i
+        tree[i][0] = 2*i            
         if 2*i+1 <= N:
             tree[i][1] = 2*i + 1
     print(tree)
-    in_order = [0]
+
+
+    in_order = [0]      # 중위 순회
     def search(node):
         if node != 0:
             search(tree[node][0])
@@ -19,6 +22,8 @@ for tc in range(1, T+1):
             search(tree[node][1])
 
     search(1)
+
+
     root = in_order.index(1)
     find_node = in_order.index(N//2)
 
