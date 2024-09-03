9082d01fe07466758caf93a384e98c993e73eb8e
clap_min99 <qkrtnals032@gmail.com>
2024-09-03 21:44:10 +0900

    sugar

--- Algorithm/algo_note(Sep)/240903/BJ_sugar_deliever.py
+++ Algorithm/algo_note(Sep)/240903/BJ_sugar_deliever.py
@@ -1,2 +1,36 @@
 # 2839 설탕배달
 
+N = int(input())
+M = N
+cnt_lst = []
+cnt = 0
+a = N//3
+b = N//5
+a_list = [i for i in range(a+1)]
+b_list = [i for i in range(b+1)]
+a_list.reverse()
+
+for i in range(len(a_list)):
+    N = N - 3*(a_list[i])
+    cnt += a_list[i]    
+    if N == 0:
+        cnt_lst.append(cnt)
+        N = M
+        cnt = 0
+    else:
+        cnt += N//5
+        N = N - 5*(N//5) 
+        if N == 0:
+            cnt_lst.append(cnt)
+            cnt = 0
+            N = M            
+        else:
+            cnt = 0
+            N = M
+            continue
+
+if cnt_lst:
+    print(min(cnt_lst))
+else:
+    print(-1)
+
