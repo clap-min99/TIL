8dff71dc1aa0351d9546bebe29550b67aab277e9
clap_min99 <qkrtnals032@gmail.com>
2024-09-19 11:19:48 +0900

    django

---
+++ django/02-template/articles/urls.py
@@ -0,0 +1,14 @@
+﻿
+from django.urls import path
+# 명시적 상대경로
+from . import views
+
+urlpatterns = [
+    path('index/', views.index),
+    path('dinner/', views.dinner),
+    path('search/', views.search),
+    path('throw/', views.throw),
+    path('catch/', views.catch),
+    path('hello/<str:name>/', views.greeting)
+]
+
