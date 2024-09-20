c691a88a869b5db778a666c95c92e84ac3354a87
clap_min99 <qkrtnals032@gmail.com>
2024-09-20 14:31:06 +0900

    240920

---
+++ Algorithm/algo_note_Sep/240920/line.py
@@ -0,0 +1,16 @@
+﻿N = int(input())
+def factorial(n):
+    if n == 0:
+        return 1
+    elif n == 1:
+        return 1
+    else:
+        return (n)*factorial(n-1)
+
+if N == 3:
+    print(0)
+elif N== 4:
+    print(1)    
+else:
+    ans = factorial(N)/(factorial(N-4)*factorial(4))
+    print(int(ans))
