2aa0a89d146ed78503b603a0ea3a2e279edb380b
clap_min99 <qkrtnals032@gmail.com>
2024-09-26 23:50:01 +0900

    0926

--- Algorithm/algo_note_Sep/240925/find_path.py
+++ Algorithm/algo_note_Sep/240925/find_path.py
@@ -1,25 +1,28 @@
 ﻿# 11403 경로찾기
+import sys
+input = sys.stdin.readline
+
 N = int(input())
 graph = [list(map(int, input().split())) for _ in range(N)]
 visited = [[0]*N for _ in range(N)]
-path = []
+# path = []
 
-# 경로 만들기
+# # 경로 만들기
+# for i in range(N):
+#     for j in range(N):
+#         if graph[i][j] == 1:
+#             path.append((i,j))
+
+
+
+def find(a, start):
+    for i in range(N):        
+        if graph[start][i] == 1 and not visited[a][i]:
+            visited[a][i] = 1
+            find(a, i)
+
 for i in range(N):
-    for j in range(N):
-        if graph[i][j] == 1:
-            path.append((i,j))
+    find(i, i)
 
-
-adj_l = [[0]*N for _ in range(N)]
-
-print(path)
-
-
-
-def find():
-    for i in range(N):
-        for j in range(N):
-            if graph[i][j] == 1 and not visited[i][j]:
-                visited[i][j] = 1
-                
+for i in visited:
+    print(*i)
