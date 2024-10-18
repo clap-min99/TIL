f33ac481d34950da6920767c82d30b2f091efe22
clap_min99 <qkrtnals032@gmail.com>
2024-10-18 17:56:54 +0900

    1018

--- Algorithm/algo_note_Oct/241018/지뢰찾기.py
+++ Algorithm/algo_note_Oct/241018/지뢰찾기.py
@@ -1,15 +1,49 @@
 # 1996 지뢰찾기
-
+from pprint import pprint
 import sys
 input = sys.stdin.readline
 
 N = int(input())
 arr = [list(input().strip()) for _ in range(N)]
-print(arr)
+
+        
+
+
 visited= [[0]*N for _ in range(N)]
 
 dij = [[1,0],[0,1],[-1,0],[0,-1],[1,1],[1,-1],[-1,-1],[-1,1]]
 
 def bomb(i, j):
-    pass
     
+    a = 0
+
+    for di, dj in dij:
+        ni = i + di
+        nj = j + dj
+        if ni < 0 or ni>= N or nj <0 or nj >= N:
+            continue
+        if arr[ni][nj].isnumeric():
+            a += int(arr[ni][nj])
+
+    if a<10:
+        visited[i][j] = a
+    else: 
+        visited[i][j] = 'M'
+
+for i in range(N):
+    for j in range(N):
+        if arr[i][j].isnumeric():
+            visited[i][j] = '*'
+        else:
+            bomb(i,j)
+
+
+for a in visited:
+    cnt = 0
+    for b in a:
+        print(b, end="")
+        cnt += 1
+        if cnt == N:
+            print()
+
+        
