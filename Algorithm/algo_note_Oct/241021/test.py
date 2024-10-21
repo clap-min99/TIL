7a0c07be651b17de332ec92b09d5884090241691
clap_min99 <qkrtnals032@gmail.com>
2024-10-22 00:02:32 +0900

    1021

--- Algorithm/algo_note_Oct/241021/test.py
+++ Algorithm/algo_note_Oct/241021/test.py
@@ -1,1 +1,11 @@
 # test
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+ans = 1
+for _ in range(N):
+    multi = int(input())
+    ans += multi-1
+
+print(ans)
---
+++ Algorithm/algo_note_Oct/241021/회의실배정.py
@@ -0,0 +1,21 @@
+# 1931 회의실 배정
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+conf = [list(map(int, input().split())) for _ in range(N)]
+cnt = 1
+conference = sorted(conf, key=lambda x:(x[1],x[0]))
+a = 0
+comp = 1
+while True:
+    if a + comp == N:
+        break
+    if conference[a][1] <= conference[a+comp][0]:
+        cnt += 1
+        a = a+comp
+        comp = 1        
+    else:
+        comp += 1
+
+print(cnt)
