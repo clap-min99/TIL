cdfc1d6559c5bc5ce0d7e891c9af26051f5b4a2a
clap_min99 <qkrtnals032@gmail.com>
2024-11-11 17:47:59 +0900

    241111

---
+++ Vue/05-routing/vue-project/src/components/UserPosts.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>UserPosts</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ Vue/05-routing/vue-project/src/views/UserView.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <h1>UserView</h1>
+        <RouterLink :to="{ name: 'user-profile'}">Profile</RouterLink>
+        <RouterLink :to="{ name: 'user-posts'}">Posts</RouterLink>
+        <h2>{{ $route.params }}번 유저 프로필 페이지</h2>
+        <h2>{{ $route.params.id }}번 유저 프로필 페이지</h2>
+        <h2>{{ userId }}번 유저 프로필 페이지</h2>
+        <button @click="goHome">홈으로!</button>
+        <button @click="routeUpdate">100번 유저b페이지로 </button>
+
+        <hr>
+        <RouterView/>
+    </div>
+
+</template>
+
+<script setup>
+import { useRoute, RouterLink, RouterView, useRouter, onBeforeRouteLeave, onBeforeRouteUpdate } from 'vue-router'
+import {ref} from 'vue'
+
+const route = useRoute()
+const userId = ref(route.params.id)
+
+const router = useRouter()
+
+const goHome = function() {
+    router.push({name: 'home'})
+    router.replace({name: 'home'})
+}
+
+onBeforeRouteLeave((to, from) => {
+    const answer = window.confirm('정말 떠나실건가요?')
+    if (answer === false) {
+        return false
+}
+})
+
+const routeUpdate = function () {
+    router.push({ name: 'user', params: {id: 100}})
+}
+
+onBeforeRouteUpdate((to, from) => {
+    userId.value = to.params.id
+})
+</script>
+
+<style scoped>
+
+</style>
