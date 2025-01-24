79fb2716e3f36bc4f7ed760548640ed7ae30691a
clap_min99 <qkrtnals032@gmail.com>
2025-01-24 17:51:02 +0900

    pjt fe

--- pororo/frontend/dev-dist/sw.js
+++ pororo/frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.j7nh8n40prg"
+    "revision": "0.6s2edubtn5"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
